d916ee440e960a46a3e386cbb462e58e806deb68
Minal Meshram <96578677+minalmeshram28@users.noreply.github.com>
2024-06-06 11:37:11 +0530

    Add files via upload

---
+++ book_db.sql
@@ -0,0 +1,15 @@
+CREATE DATABASE  book_db;
+
+USE book_db;
+
+CREATE TABLE book_form (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(100) NOT NULL,
+    email VARCHAR(100) NOT NULL,
+    phone VARCHAR(10) NOT NULL,
+    address TEXT NOT NULL,
+    location VARCHAR(100) NOT NULL,
+    guests INT NOT NULL,
+    arrivals DATE NOT NULL,
+    leaving DATE NOT NULL
+);
